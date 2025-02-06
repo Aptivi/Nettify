c51f20d8bf8934adc1f696cb4116dbc394dc132c
Aptivi CEO <eoflaoevicecity@gmail.com>
2025-02-06 18:21:20 +0300

    add - Added unit tests for mail discovery
    ---
    
    The unit tests have been added for mail discovery. This should hopefully
    reduce "updates" that fix missing mail.
    
    ---
    
    Type: add
    Breaking: False
    Doc Required: False
    Backport Required: False
    Part: 1/1

---
+++ private/Nettify.Tests/MailAddress/MailAddressTests.cs
@@ -0,0 +1,52 @@
+﻿//
+// Nettify  Copyright (C) 2023-2024  Aptivi
+//
+// This file is part of Nettify
+//
+// Nettify is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Nettify is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY, without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Nettify.MailAddress;
+using Shouldly;
+
+namespace Nettify.Tests.MailAddress
+{
+    [TestClass]
+    public sealed class MailAddressTests
+    {
+        [TestMethod]
+        public void TestListMailAddresses()
+        {
+            var ispHosts = IspTools.KnownIspHosts;
+            ispHosts.Length.ShouldBeGreaterThan(0);
+        }
+
+        [TestMethod]
+        public void TestGoogleMailCheck()
+        {
+            bool known = IspTools.IsIspKnown("gmail.com");
+            known.ShouldBe(true);
+        }
+
+        [TestMethod]
+        public void TestGoogleMailGetConfig()
+        {
+            var config = IspTools.GetIspConfigFromHost("gmail.com");
+            config.EmailProvider.ShouldNotBeNull();
+            config.EmailProvider.DisplayName.ShouldBe("Google Mail");
+            config.EmailProvider.DominatingDomain.ShouldBe("googlemail.com");
+        }
+    }
+}
