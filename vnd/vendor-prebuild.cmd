62095a80e5a55fc70d2ea55a1e0482c9f1f98507
Aptivi CEO <eoflaoevicecity@gmail.com>
2025-06-26 14:03:36 +0300

    imp - Fixed some syntax errors in prebuild
    ---
    
    We've fixed some of the syntax errors in the prebuild command.
    
    ---
    
    Type: imp
    Breaking: False
    Doc Required: False
    Backport Required: False
    Part: 1/1

--- vnd/vendor-prebuild.cmd
+++ vnd/vendor-prebuild.cmd
@@ -9,7 +9,7 @@ del /s /q "%ROOTDIR%\assets\ispdb\*"
 python3 -m venv "%ROOTDIR%\nvenv"
 call "%ROOTDIR%\nvenv\Scripts\activate.bat"
 pip install lxml
-forfiles /s /m *.xml /p "%ROOTDIRFIND%\assets\autoconfig\ispdb" /C "cmd /c echo @path && python3 ""%ROOTDIR%/assets/autoconfig/tools/convert.py"" -a -d ""%ROOTDIR%/assets/ispdb"" @path"
+forfiles /s /m *.xml /p "%ROOTDIRFIND%\assets\autoconfig\ispdb" /C "cmd /c echo @path && python3 "%ROOTDIR%/assets/autoconfig/tools/convert.py" -a -d "%ROOTDIR%/assets/ispdb" @path"
 forfiles /s /m * /p "%ROOTDIRFIND%\assets\ispdb" /C "cmd /c echo @path && move @path @path.xml"
 call "%ROOTDIR%\nvenv\Scripts\deactivate.bat"
 del /s /q "%ROOTDIR%\nvenv"
